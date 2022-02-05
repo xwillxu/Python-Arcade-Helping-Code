9eb3fd3c2e90a2b73e441c6d9b6cafa270e08460
Xwill <89924712+xwillxu@users.noreply.github.com>
2022-02-05 14:09:48 -0500

    Create Collision-Code.py

---
+++ Collision-Code.py
@@ -0,0 +1,13 @@
+import math
+
+def collision(player, ai_list):
+    for ai in ai_list:
+        if player.collides_with_sprite(ai):
+
+            ai.change_x = - player.change_x * 2
+            ai.change_y = - player.change_y * 2
+
+        if player.collides_with_sprite(ai):
+
+            player.change_x = - player.change_x * 2
+            player.change_y = - player.change_y * 2

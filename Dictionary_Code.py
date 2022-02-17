57ec490c60c8714bc6f931d91404feb7d748b87f
Xwill <89924712+xwillxu@users.noreply.github.com>
2022-02-17 12:09:48 -0500

    Create Dictionary_Code.py

---
+++ Dictionary_Code.py
@@ -0,0 +1,27 @@
+# Dictionary Code
+# Example:
+Marine_Animals_List = [ 
+  'Great_White_Shark',
+  'Dragon_Fish',
+  'Oarfish...
+]
+#⬆ This is to list The animals
+#⬇ This gives each something
+Marine_Animals_List = {
+  'Great_White_Shark':{ #⬅ Notice How The Name has To The Same As The List
+    damage: 200
+    health: 1000
+    status: 'King Of The Ocean'
+  },
+  'Dragon_Fish':{
+    damage: 500
+    health: 250
+    status: 'High Risk High Reward'
+  },
+  'Oarfish':{
+    damage: 50
+    health: 2500
+    status: 'High Health But Low Damage'
+  }...
+  
+}

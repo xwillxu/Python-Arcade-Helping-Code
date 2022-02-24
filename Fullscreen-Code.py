69640b32f90b63eecf6aca49f63ae45e81bc9a3f
Xwill <89924712+xwillxu@users.noreply.github.com>
2022-02-24 17:19:27 -0500

    Update Fullscreen-Code.py

--- Fullscreen-Code.py
+++ Fullscreen-Code.py
@@ -1,8 +1,13 @@
 # Let's Start With This Example
+
 import arcade
+
 class Example(arcade.Window):
   """Basic Example For Fullscreen Control"""
+  
   def __init__(self):
     """Where The Fullscreen Control Is"""
     # By The Way You Need The self For The __init__
-    # The Code For Fullscreen    
+    
+    # The Code For Fullscreen Control
+    self.

4b4911cdcc82329aa0804b679b890a0462a37e3d
Xwill <89924712+xwillxu@users.noreply.github.com>
2022-02-24 17:28:43 -0500

    Update Close-Window-Code.py

--- Close-Window-Code.py
+++ Close-Window-Code.py
@@ -3,4 +3,10 @@ import arcade
 
 class Example(arcade.Window):
   """Example For Close Window Code"""
-  def
+  
+  # I believe You Would Like To Press A Key To Turn Off The Example. 
+  def on_key_press(self, key, modifiers):
+        " Key Press"
+        # Fullscreen Control
+        if arcade.key.SPACE:
+            self.set_fullscreen(not self.fullscreen)
